e8c6e388203ed51fa5f961727ef985387625c21b
DennisNjeru9 <dennis.njeri@student.moringaschool.com>
2021-08-27 18:22:40 +0300

    create view functions to enable users navigate to the update profile, delete post, create new post, and create new profile templates in the blog application

--- app/main/views.py
+++ app/main/views.py
@@ -1,14 +1,16 @@
-from flask import render_template,redirect,url_for,flash,request,abort
+from flask import render_template,redirect,url_for,request
 from . import main
-from ..models import User,Comment,Post,PostLike,Subscribers
+from ..models import User,Comment,Post,Subscribers
 from .forms import PostForm,CommentForm,UpdatePostForm,UpdateProfile
 from flask_login import login_required,current_user
 from ..import db
 from ..requests import get_quote
 from ..email import mail_message,notification_message
 from datetime import datetime
+import bleach
 
-@main.route('/', methods = ['GET', 'POST'])
+
+@main.route('/', methods = ['GET','POST'])
 def index():
     posts = Post.get_all_posts()
     quote = get_quote()
@@ -20,7 +22,8 @@ def index():
         mail_message('Thank you for subscribing to the_Phi Construction Blog', 'email/welcome', new_sub.email)
     return render_template('index.html',posts = posts, quote = quote)
 
-@main.route('/post/<int:id>', methods = ['POST', 'GET'])
+
+@main.route('/post/<int:id>', methods = ['POST','GET'])
 def post(id):
     post = Post.query.filter_by(id = id).first()
     comments = Comment.query.filter_by(post_id = id).all()
@@ -41,7 +44,7 @@ def post(id):
     return render_template('post.html', post = post, comments = comments, comment_form = comment_form,comment_count = comment_count)
 
 
-@main.route("/post/<int:id>/<int:comment_id>/delete")
+@main.route('/post/<int:id>/<int:comment_id>/delete')
 def delete_comment(id, comment_id):
     post = Post.query.filter_by(id = id).first()
     comment = Comment.query.filter_by(id = comment_id).first()
@@ -49,7 +52,8 @@ def delete_comment(id, comment_id):
     db.session.commit()
     return redirect(url_for('main.post', id = post.id))
 
-@main.route("/post/<int:id>/<int:comment_id>/favourite")
+
+@main.route('/post/<int:id>/<int:comment_id>/favourite')
 def favourite_comment(id, comment_id):
     post = Post.query.filter_by(id = id).first()
     comment = Comment.query.filter_by(id = comment_id).first()
@@ -58,7 +62,8 @@ def favourite_comment(id, comment_id):
     db.session.commit()
     return redirect(url_for('main.post', id = post.id))
 
-@main.route('/post/<int:id>/update', methods = ['POST', 'GET'])
+
+@main.route('/post/<int:id>/update', methods = ['POST','GET'])
 @login_required
 def edit_post(id):
     post = Post.query.filter_by(id = id).first()
@@ -76,3 +81,68 @@ def edit_post(id):
 
     return render_template('edit_post.html', post = post, edit_form = edit_form)
 
+
+@main.route('/post/new', methods = ['POST','GET'])
+@login_required
+def new_post():
+    post_form = PostForm()
+    if post_form.validate_on_submit():
+        post_title = post_form.title.data
+        post_form.title.data = ""
+        post_content = bleach.clean(post_form.post.data, tags = bleach.sanitizer.ALLOWED_TAGS + ["h1", "h2", "h3", "h4","h5", "h6", "p", "span","div", "br", "em", "strong","i", "blockquote", "hr", "a","ul", "ol", "li"])
+        post_form.post.data = ""
+        new_post = Post(post_title = post_title,
+                        post_content = post_content,
+                        posted_at = datetime.now(),
+                        post_by = current_user.username,
+                        user_id = current_user.id)
+        new_post.save_post()
+        subs = Subscribers.query.all()
+        for sub in subs:
+            notification_message(post_title, "email/notification", sub.email, new_post = new_post)
+            pass
+        return redirect(url_for('main.post', id = new_post.id))
+    
+    return render_template('new_post.html', post_form = post_form)
+
+
+@main.route('/profile/<int:id>', methods = ['POST','GET'])
+def profile(id):
+    user = User.query.filter_by(id = id).first()
+    posts = Post.query.filter_by(user_id = id).all()
+
+    if request.method == 'POST':
+        new_sub = Subscribers(email = request.form.get("subscriber"))
+        db.session.add(new_sub)
+        db.session.commit()
+        mail_message("Thank you for subscribing to the_Phi Construction Blog", "email/welcome", new_sub.email)
+
+    return render_template('profile/profile.html',user = user,posts = posts)
+
+
+@main.route('/profile/<int:id>/<int:post_id>/delete')
+@login_required
+def delete_post(id, post_id):
+    user = User.query.filter_by(id = id).first()
+    post = Post.query.filter_by(id = post_id).first()
+    db.session.delete(post)
+    db.session.commit()
+    return redirect(url_for('main.profile', id = user.id))
+
+
+@main.route('/profile/<int:id>/update', methods = ['POST','GET'])
+@login_required
+def update_profile(id):
+    user = User.query.filter_by(id = id).first()
+    form = UpdateProfile()
+    if form.validate_on_submit():
+        user.first_name = form.first_name.data
+        user.last_name = form.last_name.data
+        user.email = form.email.data
+        user.bio = form.bio.data
+
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for('main.profile',id = id))
+    
+    return render_template('profile/update.html',user = user,form = form)
