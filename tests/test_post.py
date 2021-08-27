88e5da54ccc2502663f1f4e1ce103686ef89ba48
DennisNjeru9 <dennis.njeri@student.moringaschool.com>
2021-08-27 18:59:07 +0300

    add page structure for the navbar and base templates for application

--- tests/test_post.py
+++ tests/test_post.py
@@ -4,7 +4,7 @@ from app.models import Post, User, Comment
 class TestPost(unittest.TestCase):
     
     def setUp(self):
-        self.user_dennis = User(first_name = "Dennis",last_name = "Njeru",username = "thephi",password = "dennje349",email = "testmail@gmail.com")
+        self.user_dennis = User(username = "the_phi",password = "dennje349",email = "testmail@gmail.com")
         self.new_post = Post(post_title = "Cement",post_content = "Types of cement used in construction",user_id = self.user_dennis.id)
         self.new_comment = Comment(comment = "Great piece",post_id = self.new_post.id,user_id = self.user_dennis.id)
 
